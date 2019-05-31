0a104c07bbb6333c4ca028efbe06c234a525144b
zhangzhenhua <zhangzhenhua@gialen.com>
2019-05-31 18:02:35 +0800

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const readFilePath = 'xlsx/data.xlsx'
+if (typeof require !== 'undefined') XLSX = require('xlsx')
+const workbook = XLSX.readFile(readFilePath)
+const sheetJson = XLSX.utils.sheet_to_json(workbook.Sheets['Sheet1'])
+
+module.exports = function () {
+  return {
+    sheetJson
+  }
+}
